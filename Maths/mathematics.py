dbb787dc5a50e09a6319a38b62292819437cc35a
ECast17 <e.castorina@student.fdu.edu>
2024-04-03 10:07:33 -0400

    updated and added files

---
+++ Maths/mathematics.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def summation(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
--- server.py
+++ server.py
@@ -1,29 +1,41 @@
 from flask import Flask, render_template, request
 # Import the Maths package here
-
+from Maths.mathematics import summation, subtraction, multiplication
 app = Flask("Mathematics Problem Solver")
 
 @app.route("/sum")
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = summation(num1, num2)
+        return {"result": str(result)}, 200
+    except ValueError:
+        return {"message": "Input isn't a float number"}, 400
 
 @app.route("/sub")
 def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = subtraction(num1, num2)
+        return {"result": str(result)}, 200
+    except ValueError:
+        return {"message": "Input isn't a float number"}, 400
 
 @app.route("/mul")
 def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here  
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        result = multiplication(num1, num2)
+        return {"result": str(result)}, 200
+    except ValueError:
+        return {"message": "Invalid input type, float number is needed!"}, 400
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
